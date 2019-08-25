539aea8d905edef8bc49c3e4ef5582877b6076de
adrianstevens <adrian.stevens@gmail.com>
2019-08-25 13:38:04 -0700

    Console capture sample

---
+++ ConsoleCaptureSample/ConsoleCaptureSample/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using ConsoleOutputLib;
+
+namespace ConsoleCaptureSample
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Redirecting console output");
+            Console.WriteLine("Press any key to close");
+
+            ConsoleDataOut.Start();
+
+            // Attempt to open output file.
+            using (var writer = new StreamWriter("console_out.txt"))
+            {
+                // Redirect standard output from the console to the output file.
+                Console.SetOut(writer);
+
+                Console.ReadKey();
+            }
+
+            // Recover the standard output stream so that a 
+            // completion message can be displayed.
+            var standardOutput = new StreamWriter(Console.OpenStandardOutput());
+            standardOutput.AutoFlush = true;
+            Console.SetOut(standardOutput);
+            Console.WriteLine("Console out will be captured to console_out.txt"); 
+        }
+    }
+}
---
+++ ConsoleCaptureSample/ConsoleOutputLib/ConsoleDataOut.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleOutputLib
+{
+    public static class ConsoleDataOut
+    {
+        public static void Start ()
+        {
+          Task.Run(() =>
+          {
+              int count = 0;
+              while (count < 20)
+              {
+                  Console.WriteLine($"The count is {count++}");
+                  Thread.Sleep(500);
+              }
+          });
+        }
+    }
+}
